068439ce6782bed96d43ba1cadf33ae3041887b2
three0s <felix.y.tian@gmail.com>
2016-09-25 22:46:34 -0400

    Real  course filter and add grades

--- src/main/java/com/felixtian/uwgpa/GradeActivity.java
+++ src/main/java/com/felixtian/uwgpa/GradeActivity.java
@@ -8,18 +8,26 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GradeActivity extends AppCompatActivity {
     ArrayList<String> gradeItems;
     private String html;
+    ArrayAdapter gradeAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,21 +46,21 @@ public class GradeActivity extends AppCompatActivity {
         else{
             Log.d("oncreate","null html at grade");
         }
-        final ArrayAdapter gradeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, gradeItems);
+        gradeAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, gradeItems);
         ListView gradeListView = (ListView) findViewById(R.id.gradeList);
         gradeListView.setAdapter(gradeAdapter);
         gradeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> a, View v, int position, long id) {
-                AlertDialog.Builder adb=new AlertDialog.Builder(GradeActivity.this);
-                adb.setMessage("Delete the item?");
+                AlertDialog.Builder aDB=new AlertDialog.Builder(GradeActivity.this);
+                aDB.setMessage("Delete the item?");
                 final int positionToRemove = position;
-                adb.setNegativeButton("Cancel", null);
-                adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
+                aDB.setNegativeButton("Cancel", null);
+                aDB.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         gradeItems.remove(positionToRemove);
                         gradeAdapter.notifyDataSetChanged();
                     }});
-                adb.show();
+                aDB.show();
             }
         });
 
@@ -60,8 +68,31 @@ public class GradeActivity extends AppCompatActivity {
         fab1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                AlertDialog.Builder aDB = new AlertDialog.Builder(GradeActivity.this);
+                LayoutInflater inflater = GradeActivity.this.getLayoutInflater();
+                final View dialogView = inflater.inflate(R.layout.new_grade, null);
+                aDB.setView(dialogView);
+                final EditText subjectEdt = (EditText) dialogView.findViewById(R.id.subject);
+                final EditText gradeEdt = (EditText) dialogView.findViewById(R.id.grade);
+                aDB.setTitle("New Grade");
+                //aDB.setMessage("Enter subject and grade below");
+                aDB.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        StringBuilder sb=new StringBuilder(subjectEdt.getText().toString());
+                        String grade = gradeEdt.getText().toString();
+                        sb.append(": ");
+                        sb.append(grade);
+                        sb.append("\t\t");
+                        sb.append(GPAConvert.convert(grade));
+                        gradeItems.add(0,sb.toString());
+                        gradeAdapter.notifyDataSetChanged();
+                    }
+                });
+                aDB.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    }
+                });
+                aDB.show();
             }
         });
 
@@ -86,6 +117,37 @@ public class GradeActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    public void filter(View v){
+        AlertDialog.Builder aDB = new AlertDialog.Builder(GradeActivity.this);
+        aDB.setTitle("Filter");
+        aDB.setMessage("This will delete courses without a proper course code.");
+        aDB.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                //operate on the list... this is not good.
+                String pattern = "^.* [0-9]{3}:";
+                Pattern r = Pattern.compile(pattern);
+                ArrayList<String> newGradeItems=new ArrayList<String>();
+                for (String gradeItem: gradeItems){ //difficult to do this in-place
+                    //Log.d("regex","trying "+gradeItem);
+                    if (r.matcher(gradeItem).find()){
+                        newGradeItems.add(gradeItem);
+                        //Log.d("regex","matched "+gradeItem);
+                    }
+                }
+                gradeItems.clear();
+                for (String gradeItem: newGradeItems){
+                    gradeItems.add(gradeItem);
+                }
+                gradeAdapter.notifyDataSetChanged();
+            }
+        });
+        aDB.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+            }
+        });
+        aDB.show();
     }
 
 }
